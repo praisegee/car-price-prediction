ee753a1af09fea36dbcbc3c4bbb679e3a70ad724
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2023-07-21 22:54:23 +0100

    data preprocessing

---
+++ app.py
@@ -0,0 +1,22 @@
+import pickle
+
+import numpy as np
+
+# import sklearn
+from flask import Flask, render_template, request
+
+# from sklearn.preprocessing import StandardScaler
+
+# instanciate flask object
+app = Flask(__name__)
+# load the trained model from pickle file
+# model = pickle.load()
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
